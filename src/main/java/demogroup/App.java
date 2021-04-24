ebc71d0a27d022f06c4e60bac7baef7147400ef2
sri567 <66168680+sri567@users.noreply.github.com>
2021-04-24 10:30:38 +0530

    Add files via upload

---
+++ src/main/java/demogroup/App.java
@@ -0,0 +1,14 @@
+package demogroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!!!!" );
+        System.out.println( "web hooks test" );
+    }
+}
